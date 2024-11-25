0d62987c9377d49dff9f20ce3da08e123af37f6c
ArabWorkers <119193979+ArabWorkers@users.noreply.github.com>
2024-11-25 10:15:32 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./App.css";
 import { useEffect, useState } from "react";
-import { Input, Space, Table, Typography } from "antd";
+import { Input, Space, Table, Typography, Button, Flex } from "antd";
 import axios from "axios";
 const { Search } = Input;
 const { Title } = Typography;
@@ -33,19 +34,27 @@ const App = () => {
   const [listOfPosts, setListOfPosts] = useState([]);
   const [tablePageIndex, settablePageIndex] = useState(1);
   const [numberOfTableElements, setnumberOfTableElements] = useState(100);
-  const [searchValue, setsearchValue] = useState("");
+  const [pageSizeOfTable, setpageSizeOfTable] = useState(10);
+
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts?_page=1&_per_page=10")
+      .get("https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10")
       .then((response) => {
         setListOfPosts(response.data);
-        settablePageIndex((previousState) => {
-          return 1;
-        });
+        settablePageIndex(1);
       });
   }, []);
-  const onChange = (e) => {
-    let value = e.target.value;
+  const onResetButtonClick = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10")
+      .then((response) => {
+        setnumberOfTableElements(100);
+        setpageSizeOfTable(10);
+        settablePageIndex(1);
+        setListOfPosts(response.data);
+      });
+  };
+  const onSearch = (value) => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
       let lastResult = [];
       if (value.match(/[^0-9]/) == null && value != "") {
@@ -75,7 +84,7 @@ const App = () => {
         }
       });
       console.log(lastResult);
-      setsearchValue(value);
+      setpageSizeOfTable(lastResult.length);
       setnumberOfTableElements(lastResult.length);
       setListOfPosts(lastResult);
     });
@@ -88,24 +97,32 @@ const App = () => {
           Posts Table
         </Title>
       </Typography>
-      <Search
-        placeholder="input search text"
-        onChange={onChange}
-        style={{
-          width: 200,
-        }}
-      />
+      <Flex gap="small" wrap>
+        <Search
+          placeholder="input search text"
+          onSearch={onSearch}
+          style={{
+            width: 200,
+          }}
+        />
+        <Button type="primary" onClick={onResetButtonClick}>
+          Reset
+        </Button>
+      </Flex>
+
       <Table
         columns={columns}
         dataSource={listOfPosts}
         pagination={{
           current: tablePageIndex,
+          defaultPageSize: 10,
+          pageSize: pageSizeOfTable,
           total: numberOfTableElements,
           onShowSizeChange: (current, pageSize) => {
             let numberOfElements = pageSize * current;
             let i = 0;
             let newPageIndex = 0;
-            while (i <= numberOfTableElements) {
+            while (i <= 100) {
               i = i + pageSize;
               newPageIndex = newPageIndex + 1;
               if (numberOfElements <= i) {
@@ -118,42 +135,17 @@ const App = () => {
                 `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${pageSize}`
               )
               .then((response) => {
-                let lastResult = [];
-                if (searchValue.match(/[^0-9]/) == null && searchValue != "") {
-                  response.data.forEach((element) => {
-                    if (element.userId == searchValue) {
-                      lastResult.push(element);
-                    }
-                    if (element.id == searchValue) {
-                      if (!lastResult.includes(element)) {
-                        lastResult.push(element);
-                      }
-                    }
-                  });
-                }
-                response.data.forEach((element) => {
-                  var regex = new RegExp(searchValue, "g");
-                  if (element.title.match(regex) != null) {
-                    if (!lastResult.includes(element)) {
-                      lastResult.push(element);
-                    }
-                  }
-
-                  if (element.body.match(regex) != null) {
-                    if (!lastResult.includes(element)) {
-                      lastResult.push(element);
-                    }
-                  }
-                });
-                settablePageIndex(newPageIndex);
-                setListOfPosts(lastResult);
+                setnumberOfTableElements(100);
+                setpageSizeOfTable(pageSize);
+                console.log(response.data);
+                setListOfPosts(response.data);
               });
           },
           onChange: (page, pageSize) => {
             let numberOfElements = pageSize * page;
             let i = 0;
             let newPageIndex = 0;
-            while (i <= numberOfTableElements) {
+            while (i <= 100) {
               i = i + pageSize;
               newPageIndex = newPageIndex + 1;
               if (numberOfElements <= i) {
@@ -166,35 +158,10 @@ const App = () => {
                 `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${pageSize}`
               )
               .then((response) => {
-                let lastResult = [];
-                if (searchValue.match(/[^0-9]/) == null && searchValue != "") {
-                  response.data.forEach((element) => {
-                    if (element.userId == searchValue) {
-                      lastResult.push(element);
-                    }
-                    if (element.id == searchValue) {
-                      if (!lastResult.includes(element)) {
-                        lastResult.push(element);
-                      }
-                    }
-                  });
-                }
-                response.data.forEach((element) => {
-                  var regex = new RegExp(searchValue, "g");
-                  if (element.title.match(regex) != null) {
-                    if (!lastResult.includes(element)) {
-                      lastResult.push(element);
-                    }
-                  }
-
-                  if (element.body.match(regex) != null) {
-                    if (!lastResult.includes(element)) {
-                      lastResult.push(element);
-                    }
-                  }
-                });
+                setnumberOfTableElements(100);
+                setpageSizeOfTable(pageSize);
                 settablePageIndex(newPageIndex);
-                setListOfPosts(lastResult);
+                setListOfPosts(response.data);
               });
           },
         }}
